efe092de62a1e34343c462f303abb51c7df64c79
nebuIr <prime@nebulr.org>
2019-06-22 02:35:17 +0200

    footer now external

--- pages/news.php
+++ pages/news.php
@@ -38,7 +38,7 @@
 <main>
     <div class="grid">
         <?php
-        include_once("./../assets/php/news/getNews.php");
+        include_once("../assets/php/news/getNews.php");
         $urlPage = 1;
         $News = new getNews();
         $News->mainHtml($urlPage);
@@ -54,19 +54,8 @@
                    id="next_page" value="Load more..."/>
         </form>
 </main>
-
-<footer>
-    <p class="card-text text-light">This website was created by <img alt="nebulr" src="/assets/img/brand/pp_16.png">
-        nebulr<br>Feel
-        free to <a href="mailto:support@nebulr.me">send</a> any bug reports or feedback</p> <a
-            href="../privacypolicy/index.html">Privacy Policy</a><br>
-    <a href='https://play.google.com/store/apps/details?id=me.nebulr.atlas&utm_source=wh&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img
-                style="transform: translate(-13px, 13px)" height="75px" alt='Get it on Google Play'
-                src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
-    <a class="github-button" href="https://github.com/xnebulr" data-count-href="/xnebulr/followers" data-size="large"
-       aria-label="Follow @xnebulr on GitHub">Follow @xnebulr</a>
-    <p style="font-size: 65%" class="text-light">Google Play and the Google Play logo are trademarks of Google LLC.</p>
-</footer>
-
+<?php
+include ("../assets/html/footer.html");
+?>
 </body>
 </html>
