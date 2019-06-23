6b61dc9a66f388a95b5ceadf71bec7a003d68cd3
nebuIr <prime@nebulr.org>
2019-06-23 04:14:50 +0200

    restrict access to functions

--- functions/releases/updateReleases.php
+++ functions/releases/updateReleases.php
@@ -1,4 +1,8 @@
 <?php
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
 include_once("getReleases.php");
 $News = new getReleases();
 $News->mainSqlUpdate();
