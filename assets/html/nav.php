89cc587468846cdf188354859e71afe5f635d1e1
xnebulr <prime@nebulr.org>
2019-10-11 20:39:09 +0200

    Removed debug echo

--- assets/html/nav.php
+++ assets/html/nav.php
@@ -5,7 +5,6 @@ $url = 'https://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 <nav>
     <ul>
         <li style="margin-right: 10px; margin-top: 1px"><a href="/"><img alt="atlas logo" style="width: 50px; height: 50px; margin-top: -15px; border-radius: 15px" src="/assets/img/brand/logo_square_512px.png"/></a></li>
-        <?php echo $url ?>
         <li><a <?php if ($url == "https://atlas.nebulr.me/" || $url == "https://atlas.localhost/") {echo "class='active'";}?>href="/">Home</a></li>
         <li><a <?php if (strpos($url,'/media') !== false) {echo "class='active'";}?>href="/media">Screenshots</a></li>
         <li><a <?php if (strpos($url,'/news') !== false) {echo "class='active'";}?>href="/news">News</a></li>
